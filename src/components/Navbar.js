f08cb925ccfeae11f8b407232b4dcf4e8773b134
hamza <darejhamza@gmail.com>
2022-03-03 14:07:38 +0100

    add burger menu

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,28 +1,52 @@
-import React from 'react'
+import React,{ useState} from 'react'
 import { Link } from "react-router-dom";
 function Navbar() {
+  const [show , setShow]= useState("");
+  const [navActive , setNavActive]= useState("");
+
+const showHideNav =()=>{
+if(show==="toggle"){
+  setShow("")
+}else{
+  setShow("toggle")
+}
+if(navActive==="nav-active"){
+  setNavActive("")
+}else{
+  setNavActive("nav-active")
+}
+
+}
+
+
     return (
         
     <nav>
-      
-      <ul>
+      <Link to={process.env.PUBLIC_URL + "/"} onClick={()=>setNavActive("")}>
+          <li id="logo">HD</li>
+        </Link> 
+      <ul className={`${navActive}`}>
         
-      <Link to={process.env.PUBLIC_URL + "/"}>
+      <Link to={process.env.PUBLIC_URL + "/"} onClick={()=>setNavActive("")}>
           <li>Home</li>
         </Link>
-        <Link to="/Skills">
+        <Link to="/Skills" onClick={()=>setNavActive("")}>
           <li>Skills</li>
         </Link>
-        <Link to="/Projects">
+        <Link to="/Projects" onClick={()=>setNavActive("")}>
           <li>Projects</li>
         </Link>
-        <Link to="/Contact">
+        <Link to="/Contact" onClick={()=>setNavActive("")}>
           <li>Contact</li>
         </Link>
-        <Link to={process.env.PUBLIC_URL + "/"}>
-          <li id="logo">HD</li>
-        </Link>
+         
+ 
       </ul>
+           <div class={`burger ${show}`} onClick={()=>showHideNav()}>
+    <div class="line1"></div>
+    <div class="line2"></div>
+    <div class="line3"></div>
+</div>
     </nav>
         
     )
